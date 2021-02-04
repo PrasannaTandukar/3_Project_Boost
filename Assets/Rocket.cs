c95c2c070f73eac2715b611bf786800e7f926442
Prasanna Tandukar <pratandukar@gmail.com>
2021-02-04 11:15:09 +0545

    Added physics, keybinding and audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,15 +25,23 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))    // can thrust while rotating
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)     // so it doesnt layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
